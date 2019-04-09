bb80343c0a4e6d41b29eb05fa97dff95319e20ee
miikargh <mamoilanen@gmail.com>
2019-04-09 04:52:09 +0300

    finished

--- ex_1_1.scm
+++ ex_1_1.scm
@@ -1,14 +1,26 @@
-(display 10)
-(display "\n")
-(display (+ 5 3 4))
-(display "\n")
-(display (- 9 1))
-(display "\n")
-(display (/ 6 2))
-(display "\n")
-(display (+ (* 2 4) (- 4 6)))
-(display "\n")
-(display (define a 3))
-(display "\n")
+10
+(+ 5 3 4)
+(- 9 1)
+(/ 6 2)
+(+ (* 2 4) (- 4 6))
 
+(define a 3)
+(define b (+ a 1))
 
+(+ a b (* a b))
+(= a b)
+
+(if (and (> b a) (< b (* a b)))
+    b
+    a)
+
+(cond ((= a 4) 6)
+      ((= b 4) (+ 6 7 a))
+      (else 25))
+
+(+ 2 (if (> b a) b a))
+
+(* (cond ((> a b) a)
+         ((< a b) b)
+         (else -1))
+   (+ a 1))
