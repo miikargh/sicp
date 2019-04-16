3e86612d65f698214eca2fbcf49396ca6f9ff5dc
miikargh <mamoilanen@gmail.com>
2019-04-16 04:50:28 +0300

    Made a bit better

--- ex_1_8.scm
+++ ex_1_8.scm
@@ -4,13 +4,16 @@
      3))
 
 (define (good-enough? guess x)
-  (< (/ (abs (- (improve guess x) guess))
-        guess)
-     0.0000001))
+  (< (abs (- (improve guess x) guess))
+     (abs (* guess 0.001))))
 
 (define (cubert-iter guess x)
   (if (good-enough? guess x)
       guess
       (cubert-iter (improve guess x) x)))
 
-(cubert-iter 1.0 25.0)
+
+(define (cube-root x)
+  (cubert-iter 1.0 x))
+
+(cube-root 25.0)
